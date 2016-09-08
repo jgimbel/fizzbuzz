9b692bdefc7870ad6f7c9d5a3230c92721616703
Joel Gimbel <jgimbel@live.com>
2016-09-08 09:03:49 -0500

    configurable fizzbuzz.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,12 @@
+const operators = {
+  'Fizz': (num) => num % 3 === 0,
+  'Buzz': (num) => num % 5 === 0
+}
+
 const fizzbuzz = module.exports = (length)  => (
   Array.from(
     { length },
-    (_, idx) => (++idx % 15 === 0)
-      ? 'FizzBuzz'
-      : (idx % 3 === 0)
-        ? 'Fizz'
-        : (idx % 5 === 0)
-          ? 'Buzz'
-          : idx
+    (_, idx) => Object.keys(operators).reduce((output, key) => output + ((operators[key](idx+1)) ? key : ''), '') || idx+1
   )
 )
 
